2961c1808cfbf9cdbff8e97ef1a31769c3477245
manastyYE <abdullah.alhammadi.90160@gmail.com>
2023-11-17 00:59:57 +0300

    asdfgh

--- routes/web.php
+++ routes/web.php
@@ -47,3 +47,17 @@ Route::controller(ShowPagesController::class)->prefix('admin')->middleware('admi
 //     }
 // );
 Route::get('/login', [ShowPagesController::class,'userlogin'])->name('user.login');
+Route::get('/storage/{filename}', function ($filename) {
+    $path = storage_path('app/public/' . $filename);
+    if (!File::exists($path)) {
+        abort(404);
+    }
+
+    $file = File::get($path);
+    $type = File::mimeType($path);
+
+    $response = Response::make($file, 200);
+    $response->header("Content-Type", $type);
+
+    return $response;
+})->where('filename', '.*');
