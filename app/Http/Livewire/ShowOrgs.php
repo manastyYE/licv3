0e94fc5bc0f3df6f9c9b00bf74c20c26e1f9c2d3
manastyYE <abdullah.alhammadi.90160@gmail.com>
2023-11-28 16:44:08 +0300

    Update ShowOrgs.php

--- app/Http/Livewire/ShowOrgs.php
+++ app/Http/Livewire/ShowOrgs.php
@@ -12,12 +12,13 @@ class ShowOrgs extends Component
     public function render()
     {
         $orgs=Org::all();
-        $orgs = $this->search ? Org::where('org_name', 'like', '%' . $this->search . '%')
+        $orgs = $this->search ? Org::orderBy('created_at', 'desc')->where('org_name', 'like', '%' . $this->search . '%')
             ->orWhere('owner_name', 'like', '%' . $this->search . '%')
             ->orWhere('owner_phone', 'like', '%' . $this->search . '%')
 
+
             ->paginate(8)
-            : Org::paginate(8);
+            : Org::orderBy('created_at', 'desc')->paginate(8);
 
         return view('livewire.show-orgs',['type'=>$orgs ]);
     }
