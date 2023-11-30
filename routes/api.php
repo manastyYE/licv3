be8918a64b9470b97ad863ad94bc0fd09e642640
GamalGamil66 <135033603+GamalGamil66@users.noreply.github.com>
2023-11-30 21:27:15 +0300

    uap

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('get_streets',[\App\Http\Controllers\Api\UserDataContoller::class,'get_streets']);
 Route::get('get_orgs',[\App\Http\Controllers\Api\UserDataContoller::class,'get_orgs']);
-Route::get('get_org',[\App\Http\Controllers\Api\UserDataContoller::class,'get_org']);
+Route::post('get_org',[\App\Http\Controllers\Api\UserDataContoller::class,'get_org']);
 Route::post('login', [AuthController::class,'login']);
 
 
