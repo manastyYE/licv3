18a0d78950285cd9b54b55e9ab9ba1bb92b648cd
manastyYE <137563111+manastyYE@users.noreply.github.com>
2023-12-25 17:50:27 +0300

    Update System_initialization.blade.php

--- resources/views/dashboard/admin/System_initialization.blade.php
+++ resources/views/dashboard/admin/System_initialization.blade.php
@@ -133,6 +133,25 @@
                     </div>
                 </div>
             </a>
+            <a href="/admin/building">
+                <div class="card">
+                    <div class="flex justify-center p-5">
+                        <img class="w-9/12" src="{{ asset('icons/add_shop.png') }}" alt="image">
+                    </div>
+                    <div class="px-4 pb-8 text-center sm:px-5">
+                        <h4 class="text-lg font-semibold text-slate-700 dark:text-navy-100">
+                            انواع البناء
+                        </h4>
+                        <p class="pt-3">
+
+                        </p>
+                        {{-- <button
+                        class="mt-8 font-medium text-white shadow-lg btn bg-primary shadow-primary/50 hover:bg-primary-focus focus:bg-primary-focus active:bg-primary-focus/90 dark:bg-accent dark:shadow-accent/50 dark:hover:bg-accent-focus dark:focus:bg-accent-focus dark:active:bg-accent/90">
+
+                    </button> --}}
+                    </div>
+                </div>
+            </a>
         </div>
         {{-- @livewire('users') --}}
         {{-- <livewire:users> --}}
