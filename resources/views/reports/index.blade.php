7360452f3a4ce7735eef939bd7e174330132cd02
manastyYE <137563111+manastyYE@users.noreply.github.com>
2023-12-27 16:30:03 +0300

    report

--- resources/views/reports/index.blade.php
+++ resources/views/reports/index.blade.php
@@ -1,21 +1,291 @@
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </head>
+
 <body dir="rtl">
+    <div class="   ">
+        <table class="table table-border text-center">
+            <tr class="table-warning">
+                <td>
+                    <h3>
+                        حافظة النظام الالي
+                    </h3>
+                </td>
+            </tr>
+            <tr>
+                <td scope="col">
+                    بيانات تعريفية
+                </td>
+            </tr>
+        </table>
+        <table class="table table-bordered text-center">
+            <tr>
+                <td class="bg-info col-1">
+                الاسم رباعيا
+                </td>
+                <td class="col-3">
 
-<h1 style="
-direction: rtl
-font-family: 'Times New Roman', Times, serif;
- background-color:saddlebrown" >
- alhammadi soft
+                    <!-- الاسم الرباعي -->
+                </td>
+                <td class="bg-info col-2">
+                    رقم التلفون
+                </td>
+                <td>
+                    <!-- رقم الهاتف -->
+                </td>
+                <td class="bg-info col-2 ">
+                    اسم الشهرة
+                </td>
+                <td>
+                    <!-- اسم الشهرة -->
+                </td>
 
-</h1>
+            </tr>
+            <tr>
+                <td class="bg-info col-2">
+                    نوع النشاط
+                </td>
+                <td>
+
+                    <!-- نوع النشاط -->
+                </td>
+                <td class="bg-info col-2">
+                    العنوان
+                </td>
+                <td>
+                    <!-- العنوان -->
+                </td>
+                <td class="bg-info col-2">
+                    الحي
+                </td>
+                <td>
+                    <!-- الحي -->
+                </td>
+
+            </tr>
+
+        </table>
+        <table class="table text-center ">
+            <tr>
+                <td class="">
+                    بيانات اللوحة الدعاية
+                </td>
+            </tr>
+            <tr>
+                <td class=" bg-info">
+                    بيانات اللوحة
+                </td>
+            </tr>
+        </table>
+        <table class="table table-bordered text-center " style="margin-top: -5mm;">
+
+            <tr class="bg-info">
+                <td>
+                    امامي
+                </td>
+                <td>
+                    جانبي
+                </td>
+                <td>
+                    جداري
+                </td>
+                <td>
+                    سطحية
+                </td>
+                <td>
+                    حروف
+                </td>
+                <td>
+                    لواصق
+                </td>
+                <td>
+                    غيرها
+                </td>
+            </tr>
+            <tr class="">
+                <td>
+                    0
+                    <!-- امامي -->
+                </td>
+                <td>
+                    <!-- جانبي -->
+                </td>
+                <td>
+                    <!-- جداري -->
+                </td>
+                <td>
+                    <!-- سطحية -->
+                </td>
+                <td>
+                    <!-- حروف -->
+                </td>
+                <td>
+                    <!-- لواصق -->
+                </td>
+                <td>
+                    <!--     -->
+                </td>
+            </tr>
+
+
+
+        </table>
+        <table class="table text-center"> <tr>
+            <td>
+                بيانات مالية مستحقة
+            </td>
+        </tr></table>
+        <table class="table table-bordered text-center" style="margin-top: -5mm;" >
+
+                <tr class="bg-info">
+                    <td class="col-2">
+                        الرسوم
+                    </td>
+                    <td class="col-2">
+                        المبلغ
+                    </td>
+                    <td class="col-4">
+                        ارقام السندات
+                    </td>
+                    <td class="col-2">
+                        التاريخ
+                    </td>
+                    <td class="col-2">
+
+                        ملاحظات
+                    </td>
+                </tr>
+                <tr class="">
+                    <td class="col-2 bg-info">
+                        رسوم محلية
+                    </td>
+                    <td class="col-2">
+                        <!-- المبلغ -->
+                    </td>
+                    <td class="col-4">
+                        <!-- ارقام السندات -->
+                    </td>
+                    <td class="col-2">
+                        <!-- التاريخ -->
+                    </td>
+                    <td class="col-2">
+
+                        <!-- ملاحظات -->
+                    </td>
+                </tr>
+                <tr class="">
+                    <td class="col-2 bg-info">
+                        اخرى
+                    </td>
+                    <td class="col-2">
+                        <!-- المبلغ -->
+                    </td>
+                    <td class="col-4">
+                        <!-- ارقام السندات -->
+                    </td>
+                    <td class="col-2">
+                        <!-- التاريخ -->
+                    </td>
+                    <td class="col-2">
+
+                        <!-- ملاحظات -->
+                    </td>
+                </tr>
+                <tr class="">
+                    <td class="col-2 bg-info">
+                        دعاية واعلان
+                    </td>
+                    <td class="col-2">
+                        <!-- المبلغ -->
+                    </td>
+                    <td class="col-4">
+                        <!-- ارقام السندات -->
+                    </td>
+                    <td class="col-2">
+                        <!-- التاريخ -->
+                    </td>
+                    <td class="col-2">
+
+                        <!-- ملاحظات -->
+                    </td>
+                </tr>
+                <tr class="">
+                    <td class="col-2 bg-info">
+                        نظافة
+                    </td>
+                    <td class="col-2">
+                        <!-- المبلغ -->
+                    </td>
+                    <td class="col-4">
+                        <!-- ارقام السندات -->
+                    </td>
+                    <td class="col-2">
+                        <!-- التاريخ -->
+                    </td>
+                    <td class="col-2">
+
+                        <!-- ملاحظات -->
+                    </td>
+                </tr>
+                <tr class="">
+                    <td class="col-2 bg-info">
+                        نظافة مهن
+                    </td>
+                    <td class="col-2">
+                        <!-- المبلغ -->
+                    </td>
+                    <td class="col-4">
+                        <!-- ارقام السندات -->
+                    </td>
+                    <td class="col-2">
+                        <!-- التاريخ -->
+                    </td>
+                    <td class="col-2">
+
+                        <!-- ملاحظات -->
+                    </td>
+                </tr>
+                <tr class="">
+                    <td class="col-2 bg-info">
+                        الاجمالي
+                    </td>
+                    <td class="col-2">
+                        <!-- المبلغ -->
+                    </td>
+                    <td colspan="3" >
+                        <!-- ارقام السندات -->
+                    </td>
+
+                </tr>
+        </table>
+        <table class="table text-center">
+            <tr>
+                <td>
+                    النظام الالي
+                </td>
+                <td>
+                    المفتش
+                </td>
+                <td>
+                    مندوب الصندوق
+                </td>
+                <td>
+                    رئيس القسم
+                </td>
+                <td>
+                    النائب للبلديات
+                </td>
+            </tr>
+        </table>
+
+    </div>
 </body>
+
 </html>
