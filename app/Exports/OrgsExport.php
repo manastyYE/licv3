2439dae848f9b4cbadd5affb8b7376d80fc0d73d
GamalGamil66 <135033603+GamalGamil66@users.noreply.github.com>
2023-12-29 01:41:27 +0300

    excel

---
+++ app/Exports/OrgsExport.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Exports;
+
+use App\Models\Org;
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\FromQuery;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
+use Maatwebsite\Excel\Concerns\WithCustomStartCell;
+
+class OrgsExport implements FromQuery, WithHeadings, WithMapping, WithCustomStartCell
+{
+    /**
+    * @return \Illuminate\Support\Collection
+    */
+    // public function collection()
+    // {
+    //     return Org::all();
+    // }
+
+    protected $users;
+
+    public function __construct($users)
+    {
+        $this->users = $users;
+    }
+
+    public function query()
+    {
+        return Org::with(['street','org_type'])->whereIn('id', $this->users);
+    }
+
+    public function headings(): array
+    {
+        return [
+            'ID',
+            'org_name',
+            'owner_name',
+            'owner_phone',
+            'card_number',
+            'street',
+            'org_type',
+        ];
+    }
+
+    public function map($user): array
+    {
+        return [
+            $user->id,
+            $user->org_name,
+            $user->owner_name,
+            $user->owner_phone,
+            $user->card_number,
+            $user->street->name,
+            $user->org_type->name,
+        ];
+    }
+    public function startCell(): string
+    {
+        return 'A1'; // Set the start cell to A1
+    }
+}
--- app/Http/Livewire/ShowOrgs.php
+++ app/Http/Livewire/ShowOrgs.php
@@ -2,16 +2,20 @@
 
 namespace App\Http\Livewire;
 
+use App\Exports\OrgsExport;
 use Livewire\Component;
 use App\Models\Org;
 use Livewire\WithPagination;
+use Maatwebsite\Excel\Facades\Excel;
+
 class ShowOrgs extends Component
 {
     use WithPagination;
     public $search;
+    public $selectedUserIds = [];
     public function render()
     {
-        
+
         $orgs = $this->search ? Org::orderBy('created_at', 'desc')->where('org_name', 'like', '%' . $this->search . '%')
             ->orWhere('owner_name', 'like', '%' . $this->search . '%')
             ->orWhere('owner_phone', 'like', '%' . $this->search . '%')
@@ -20,7 +24,16 @@ class ShowOrgs extends Component
             ->paginate(8)
             : Org::orderBy('created_at', 'desc')->paginate(8);
 
+            $this->selectedUserIds = $orgs->pluck('id');
+
         return view('livewire.show-orgs',['type'=>$orgs ]);
+    }
+
+    public function export()
+    {
+        $users = $this->selectedUserIds;
+        return Excel::download(new OrgsExport($users), 'users.xlsx');
+        // return Excel::download(new OrgsExport, 'users.xlsx');
     }
 
 }
