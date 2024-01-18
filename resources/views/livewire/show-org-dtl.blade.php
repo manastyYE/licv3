d3ad80bf508ba6b5fa0e5cb10cbdcc68cefdbb04
manastyYE <137563111+manastyYE@users.noreply.github.com>
2024-01-18 17:45:45 +0300

    Update show-org-dtl.blade.php

--- resources/views/livewire/show-org-dtl.blade.php
+++ resources/views/livewire/show-org-dtl.blade.php
@@ -379,7 +379,7 @@
                         </button>
 
                         @else
-                        @if($can_have_clip == 0 && $clip)
+                        @if($can_have_clip == 0 )
                             <button type="button" x-data x-on:click="$dispatch('open-modal',{name:'add-org-clip-modal'})"
                         class="font-medium btn bg-slate-150 text-slate-800 hover:bg-slate-200 focus:bg-slate-200 active:bg-slate-200/80 dark:bg-navy-500 dark:text-navy-50 dark:hover:bg-navy-450 dark:focus:bg-navy-450 dark:active:bg-navy-450/90">
                         انشاء حافظة
