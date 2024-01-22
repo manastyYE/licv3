c0698975ad2dd44a0399bbaa23ef01b830155646
GamalGamil66 <135033603+GamalGamil66@users.noreply.github.com>
2024-01-22 19:52:30 +0300

    viroeg2

--- app/Http/Livewire/VirOrgs/VirOrgs.php
+++ app/Http/Livewire/VirOrgs/VirOrgs.php
@@ -61,7 +61,7 @@ class VirOrgs extends Component
         $this->selectedUserIds = $this->search ?ModelsVirOrgs::with(['street','org_type','user'])->orderBy('created_at', 'desc')
         ->where('org_name','like','%'.$this->search . '%')
         ->where('user_id',$this->worker_id)
-        ->orwhere('owner_name','like','%'.$this->search . '%')->pluck('id') :ModelsVirOrgs::with(['street','org_type','user'])->orderBy('created_at', 'desc')
+        ->orwhere('owner_name','like','%'.$this->search . '%')->pluck('id') :ModelsVirOrgs::with(['street','org_type','user'])->where('user_id',$this->worker_id)->orderBy('created_at', 'desc')
         ->pluck('id');
         }else{
             $this->selectedUserIds = $this->search ?ModelsVirOrgs::with(['street','org_type','user'])->orderBy('created_at', 'desc')
